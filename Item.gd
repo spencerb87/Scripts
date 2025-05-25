4843d17db874650f46f14bb0cd070c7eaf2b9e86
spencerb87 <spencer.ball@gmail.com>
2025-05-24 21:00:52 -0400

    item pickup changes

--- Item.gd
+++ Item.gd
@@ -14,7 +14,7 @@ extends Resource
 @export var is_melee: bool = false
 @export var is_throwable: bool = false
 @export var max_stack_size: int = 1
-@export var item_scene: PackedScene = null  # For 3D representation when dropped in world
+#@export var item_scene: PackedScene = null  # For 3D representation when dropped in world
 
 
 # Constructor to easily create items in code
@@ -27,11 +27,11 @@ func _init(p_id: String = "", p_name: String = "", p_desc: String = "", p_icon: 
 	icon = p_icon
 	stackable = p_stackable
 	max_stack_size = p_max_stack
-	item_scene = p_scene
+	#item_scene = p_scene
 	is_equipment = p_is_equipment
 
 # Method to create a copy of this item
-func duplicate_item() -> Item:
-	# Creates a new instance with the same properties
-	var new_item = Item.new(id, item_name, description, icon, stackable, max_stack_size, item_scene, is_equipment)
-	return new_item
+#func duplicate_item() -> Item:
+	## Creates a new instance with the same properties
+	#var new_item = Item.new(id, item_name, description, icon, stackable, max_stack_size, item_scene, is_equipment)
+	#return new_item
